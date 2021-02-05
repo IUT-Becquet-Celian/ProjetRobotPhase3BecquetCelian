882e17922eaddb3f9ea0192f0fa4e16914d3b510
Valentin <vgies@univ-tln.fr>
2021-02-05 15:19:34 +0100

    Initial commit - remontée odo ok - pas de pilotage moteur

---
+++ StrategyManagerProjetEtudiant/StrategyEurobot.cs
@@ -0,0 +1,56 @@
+﻿using Constants;
+using EventArgsLibrary;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using Utilities;
+using WorldMap;
+
+namespace StrategyManagerProjetEtudiantNS
+{
+    public class StrategyEurobot : StrategyGenerique
+    {
+        Stopwatch sw = new Stopwatch();
+
+        public PointD robotDestination = new PointD(0, 0);
+        PlayingSide playingSide = PlayingSide.Left;     
+
+        TaskDemoMove taskDemoMove;
+        TaskDemoMessage taskDemoMessage;
+
+        //Timer configTimer;
+
+        public StrategyEurobot(int robotId, int teamId, string multicastIpAddress) : base(robotId, teamId, multicastIpAddress)
+        {
+            taskDemoMove = new TaskDemoMove(this);
+            taskDemoMessage = new TaskDemoMessage(this);
+        }
+
+        public override void InitStrategy()
+        {
+            //Obtenus directement à partir du script Matlab
+            OnOdometryPointToMeter(1.178449e-06);
+            On2WheelsAngleSetup(-1.570796e+00, 1.570796e+00);
+            On2WheelsToPolarSetup(5.000000e-01, -5.000000e-01,
+                                4.166667e+00, 4.166667e+00);
+        }
+        
+        public override void IterateStateMachines()
+        {
+            
+        }
+        
+
+        /*********************************** Events reçus **********************************************/
+        
+
+        /*********************************** Events de sortie **********************************************/
+        
+    }
+       
+
+}
